d6f23ac2bb3921a0519a8cb7fa2d99efc0f539f7
GabyBonfim <gabrielybonfim05@gmail.com>
2025-06-04 17:02:21 -0300

    modificações finais

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+    function filtrarFAQ() {
+      const input = document.getElementById("pesquisa").value.toLowerCase();
+      const perguntas = document.querySelectorAll(".faq-card");
+
+      perguntas.forEach(card => {
+        const texto = card.textContent.toLowerCase();
+        card.style.display = texto.includes(input) ? "block" : "none";
+      });
+    }
+
+    function filtrarLocal() {
+      const input = document.getElementById("input-pesquisa").value.toLowerCase();
+      const perguntas = document.querySelectorAll(".card-endereco");
+
+      perguntas.forEach(card => {
+        const texto = card.textContent.toLowerCase();
+        card.style.display = texto.includes(input) ? "flex" : "none";
+      });
+    }
+
+    
+
+function toggleSideMenu() {
+  const menu = document.getElementById('side-menu');
+  const isOpen = menu.style.left === '0px';
+  menu.style.left = isOpen ? '-250px' : '0px';
+}
+
+document.addEventListener('click', function(event) {
+  const menu = document.getElementById('side-menu');
+  const button = document.getElementById('menu-button');
+
+  if (menu.style.left === '0px' && !menu.contains(event.target) && !button.contains(event.target)) {
+    menu.style.left = '-250px';
+  }
+});
+document.getElementById('close-menu').addEventListener('click', function() {
+  toggleSideMenu();
+});
